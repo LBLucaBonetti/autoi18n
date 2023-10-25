099726ced6ecef73177cd60ebf32079a83222517
Luca <lb.luca.bonetti@gmail.com>
2023-10-25 22:31:47 +0200

    fix(main-options-and-parameters): blank region

--- src/main/java/it/lbsoftware/autoi18n/converters/LocaleTypeConverter.java
+++ src/main/java/it/lbsoftware/autoi18n/converters/LocaleTypeConverter.java
@@ -18,7 +18,7 @@ public class LocaleTypeConverter implements ITypeConverter<Locale> {
   private Locale toLocale(String value) {
     var split = value.split("-");
     var localeBuilder = new Locale.Builder().setLanguage(split[0]);
-    if (split.length > 1) {
+    if (split.length > 1 && StringUtils.isNotBlank(split[1])) {
       localeBuilder.setRegion(split[1]);
     }
     return localeBuilder.build();
