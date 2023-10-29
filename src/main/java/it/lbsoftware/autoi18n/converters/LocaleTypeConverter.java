eb7e3889a8a5001e8a55b12b583b15755015866d
Luca <lb.luca.bonetti@gmail.com>
2023-10-29 18:44:14 +0100

    feat(google-cloud-translation-v3): add method to get language and region from locale

--- src/main/java/it/lbsoftware/autoi18n/converters/LocaleTypeConverter.java
+++ src/main/java/it/lbsoftware/autoi18n/converters/LocaleTypeConverter.java
@@ -2,10 +2,24 @@ package it.lbsoftware.autoi18n.converters;
 
 import java.util.Locale;
 import java.util.Optional;
+import lombok.NonNull;
 import org.apache.commons.lang3.StringUtils;
 import picocli.CommandLine.ITypeConverter;
 
 public class LocaleTypeConverter implements ITypeConverter<Locale> {
+
+  public static String getLanguageAndRegion(@NonNull final Locale locale) {
+    return Optional.ofNullable(locale.getLanguage())
+        .filter(StringUtils::isNotBlank)
+        .map(
+            (String language) ->
+                language
+                    + Optional.ofNullable(locale.getCountry())
+                        .filter(StringUtils::isNotBlank)
+                        .map((String country) -> "-" + country)
+                        .orElse(StringUtils.EMPTY))
+        .orElse(StringUtils.EMPTY);
+  }
 
   @Override
   public Locale convert(final String value) {
