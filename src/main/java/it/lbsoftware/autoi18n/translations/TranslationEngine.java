404b8fdc91208aa8451f4896169de5b3d768f06c
Luca <lb.luca.bonetti@gmail.com>
2023-10-29 18:40:43 +0100

    feat(google-cloud-translation-v3): add translation service to engine

--- src/main/java/it/lbsoftware/autoi18n/translations/TranslationEngine.java
+++ src/main/java/it/lbsoftware/autoi18n/translations/TranslationEngine.java
@@ -7,7 +7,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum TranslationEngine {
-  GOOGLE_CLOUD_TRANSLATION_V3("Google Cloud Translation v3");
+  GOOGLE_CLOUD_TRANSLATION_V3(null, "Google Cloud Translation v3");
 
+  private final TranslationService translationService;
   private final String name;
 }
