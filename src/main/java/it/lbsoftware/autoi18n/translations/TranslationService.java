8095e502ff0e899d62e72179fbe4bdb055e6912c
Luca <lb.luca.bonetti@gmail.com>
2023-10-31 13:14:49 +0100

    feat(google-cloud-translation-v3): add translation engine params object to translation service

--- src/main/java/it/lbsoftware/autoi18n/translations/TranslationService.java
+++ src/main/java/it/lbsoftware/autoi18n/translations/TranslationService.java
@@ -1,5 +1,6 @@
 package it.lbsoftware.autoi18n.translations;
 
+import it.lbsoftware.autoi18n.utils.TranslationEngineParams;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
@@ -13,9 +14,13 @@ public interface TranslationService {
    * @param entries Key-value pairs to translate
    * @param inputLocale The source language of the entries
    * @param outputLocales The target languages for translations
+   * @param translationEngineParams The additional parameters for the translation engine
    * @return A data structure associating each target language with its corresponding set of
    *     key-value pairs translated
    */
   Map<Locale, Map<String, String>> translate(
-      Map<String, String> entries, Locale inputLocale, Set<Locale> outputLocales);
+      Map<String, String> entries,
+      Locale inputLocale,
+      Set<Locale> outputLocales,
+      TranslationEngineParams translationEngineParams);
 }
