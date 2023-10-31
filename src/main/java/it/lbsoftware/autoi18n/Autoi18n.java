90796e8b15264f385b2c02a8e89b7f59885a3b11
Luca <lb.luca.bonetti@gmail.com>
2023-10-31 13:29:16 +0100

    feat(google-cloud-translation-v3): add translation engine to main command

--- src/main/java/it/lbsoftware/autoi18n/Autoi18n.java
+++ src/main/java/it/lbsoftware/autoi18n/Autoi18n.java
@@ -5,15 +5,19 @@ import static it.lbsoftware.autoi18n.constants.Constants.OPTION_LONG_ENTRY;
 import static it.lbsoftware.autoi18n.constants.Constants.OPTION_LONG_INPUT_LANGUAGE;
 import static it.lbsoftware.autoi18n.constants.Constants.OPTION_LONG_OUTPUT_LANGUAGES;
 import static it.lbsoftware.autoi18n.constants.Constants.OPTION_LONG_TRANSLATION_ENGINE;
+import static it.lbsoftware.autoi18n.constants.Constants.OPTION_LONG_TRANSLATION_ENGINE_PARAMS;
 import static it.lbsoftware.autoi18n.constants.Constants.OPTION_SHORT_ENTRY;
 import static it.lbsoftware.autoi18n.constants.Constants.OPTION_SHORT_INPUT_LANGUAGE;
 import static it.lbsoftware.autoi18n.constants.Constants.OPTION_SHORT_OUTPUT_LANGUAGES;
 import static it.lbsoftware.autoi18n.constants.Constants.OPTION_SHORT_TRANSLATION_ENGINE;
 
 import it.lbsoftware.autoi18n.converters.LocaleTypeConverter;
+import it.lbsoftware.autoi18n.converters.TranslationEngineParamsConverter;
 import it.lbsoftware.autoi18n.translations.TranslationEngine;
+import it.lbsoftware.autoi18n.utils.TranslationEngineParams;
 import it.lbsoftware.autoi18n.utils.VersionProvider;
-import java.util.LinkedHashSet;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.concurrent.Callable;
@@ -40,14 +44,14 @@ public class Autoi18n implements Callable<Integer> {
       split = ",",
       splitSynopsisLabel = ",",
       converter = LocaleTypeConverter.class)
-  private LinkedHashSet<Locale> outputLocales;
+  private List<Locale> outputLocales;
 
   @Getter
   @Option(
       names = {OPTION_SHORT_ENTRY, OPTION_LONG_ENTRY},
       required = true,
       description =
-          "Key-value item(s) to translate; if multiple key-value pairs are specified, they need to share a common source language (<input-language> option); an unspecified or blank value will not be translated nor inserted into language files.",
+          "Key-value item(s) to translate; if multiple key-value pairs are specified, they need to share a common source language (<input-language> option); an unspecified or blank value will not be translated nor inserted into language files; whenever an error occurs during the translation of one or multiple entries, the resulting output string will be empty",
       paramLabel = "<key>=<value>",
       mapFallbackValue = StringUtils.EMPTY,
       split = ",",
@@ -68,15 +72,36 @@ public class Autoi18n implements Callable<Integer> {
   @Option(
       names = {OPTION_SHORT_TRANSLATION_ENGINE, OPTION_LONG_TRANSLATION_ENGINE},
       required = false,
-      description = "The translation engine to use",
+      description =
+          """
+The translation engine to use; the following engines are actually available:
+- Google Cloud Translation v3
+  Parameters via --translation-engine-params:
+  api-key: the API key to use
+  project-number-or-id: the Google Cloud project or id to use
+""",
       paramLabel = "<translationEngine>")
   private TranslationEngine translationEngine = TranslationEngine.GOOGLE_CLOUD_TRANSLATION_V3;
+
+  @Getter
+  @Option(
+      names = {OPTION_LONG_TRANSLATION_ENGINE_PARAMS},
+      required = false,
+      description =
+          "Additional parameters to customize the translation engine; see <translationEngine> description for more details",
+      paramLabel = "<key>=<value>",
+      converter = TranslationEngineParamsConverter.class)
+  private TranslationEngineParams translationEngineParams = new TranslationEngineParams();
 
   @Spec private CommandSpec commandSpec;
 
   @Override
   public Integer call() {
-    commandSpec.commandLine().usage(commandSpec.commandLine().getOut());
+    System.out.println(
+        translationEngine
+            .getTranslationService()
+            .translate(
+                entries, inputLocale, new HashSet<>(outputLocales), translationEngineParams));
     return 0;
   }
 }
