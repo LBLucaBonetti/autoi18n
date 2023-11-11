eaebfb404b77714059cf97b90bab1ac204a2aed5
Luca <lb.luca.bonetti@gmail.com>
2023-11-11 12:58:34 +0100

    refactor(translation-services): move methods to super class

--- src/main/java/it/lbsoftware/autoi18n/translations/AbstractTranslationService.java
+++ src/main/java/it/lbsoftware/autoi18n/translations/AbstractTranslationService.java
@@ -1,5 +1,14 @@
 package it.lbsoftware.autoi18n.translations;
 
+import it.lbsoftware.autoi18n.paramsproviders.TranslationEngineParams;
+import it.lbsoftware.autoi18n.utils.LanguageAndCountry;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
+import lombok.NonNull;
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * A translation service wraps and handles the logic required to call the translation calls to the
  * service provider API, using a {@code TranslationApi}
@@ -14,4 +23,65 @@ public abstract class AbstractTranslationService<O, I> implements TranslationSer
   protected AbstractTranslationService(final TranslationApi<O, I> translationApi) {
     this.translationApi = translationApi;
   }
+
+  @Override
+  public Map<LanguageAndCountry, Map<String, String>> translate(
+      @NonNull final Map<String, String> entries,
+      @NonNull final LanguageAndCountry inputLanguageAndCountry,
+      @NonNull final Set<LanguageAndCountry> outputLanguageAndCountries,
+      @NonNull final TranslationEngineParams translationEngineParams) {
+    var translations = new HashMap<LanguageAndCountry, Map<String, String>>();
+    outputLanguageAndCountries.forEach(
+        (LanguageAndCountry outputLanguageAndCountry) ->
+            translations.put(
+                outputLanguageAndCountry,
+                translate(
+                    entries,
+                    inputLanguageAndCountry,
+                    outputLanguageAndCountry,
+                    translationEngineParams)));
+    return translations;
+  }
+
+  private Map<String, String> translate(
+      final Map<String, String> entries,
+      final LanguageAndCountry inputLanguageAndCountry,
+      final LanguageAndCountry outputLanguageAndCountry,
+      final TranslationEngineParams translationEngineParams) {
+    var translations = new HashMap<String, String>();
+    entries.forEach(
+        (key, value) ->
+            translations.put(
+                key,
+                StringUtils.isBlank(value)
+                    ? StringUtils.EMPTY
+                    : translate(
+                        value,
+                        inputLanguageAndCountry,
+                        outputLanguageAndCountry,
+                        translationEngineParams)));
+    return translations;
+  }
+
+  private String translate(
+      final String entry,
+      final LanguageAndCountry inputLanguageAndCountry,
+      final LanguageAndCountry outputLanguageAndCountry,
+      final TranslationEngineParams translationEngineParams) {
+    String inputLanguage = inputLanguageAndCountry.toString();
+    String outputLanguage = outputLanguageAndCountry.toString();
+    if (Stream.of(inputLanguage, outputLanguage).allMatch(StringUtils::isNotBlank)) {
+      return translate(entry, inputLanguage, outputLanguage, translationEngineParams);
+    }
+    System.out.printf(
+        "Both input and output languages should not be blank; inputLanguage: %s, outputLanguage: %s%n",
+        inputLanguage, outputLanguage);
+    return StringUtils.EMPTY;
+  }
+
+  protected abstract String translate(
+      final String entry,
+      final String inputLanguage,
+      final String outputLanguage,
+      final TranslationEngineParams translationEngineParams);
 }
--- src/main/java/it/lbsoftware/autoi18n/translations/impl/libretranslate/LibreTranslateService.java
+++ src/main/java/it/lbsoftware/autoi18n/translations/impl/libretranslate/LibreTranslateService.java
@@ -5,12 +5,6 @@ import it.lbsoftware.autoi18n.translations.AbstractTranslationService;
 import it.lbsoftware.autoi18n.translations.TranslationApi;
 import it.lbsoftware.autoi18n.translations.impl.libretranslate.pojos.LibreTranslateRequest;
 import it.lbsoftware.autoi18n.translations.impl.libretranslate.pojos.LibreTranslateResponse;
-import it.lbsoftware.autoi18n.utils.LanguageAndCountry;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Stream;
-import lombok.NonNull;
 import org.apache.commons.lang3.StringUtils;
 
 public final class LibreTranslateService
@@ -22,61 +16,7 @@ public final class LibreTranslateService
   }
 
   @Override
-  public Map<LanguageAndCountry, Map<String, String>> translate(
-      @NonNull final Map<String, String> entries,
-      @NonNull final LanguageAndCountry inputLanguageAndCountry,
-      @NonNull final Set<LanguageAndCountry> outputLanguageAndCountries,
-      @NonNull final TranslationEngineParams translationEngineParams) {
-    var translations = new HashMap<LanguageAndCountry, Map<String, String>>();
-    outputLanguageAndCountries.forEach(
-        (LanguageAndCountry outputLanguageAndCountry) ->
-            translations.put(
-                outputLanguageAndCountry,
-                translate(
-                    entries,
-                    inputLanguageAndCountry,
-                    outputLanguageAndCountry,
-                    translationEngineParams)));
-    return translations;
-  }
-
-  private Map<String, String> translate(
-      final Map<String, String> entries,
-      final LanguageAndCountry inputLanguageAndCountry,
-      final LanguageAndCountry outputLanguageAndCountry,
-      final TranslationEngineParams translationEngineParams) {
-    var translations = new HashMap<String, String>();
-    entries.forEach(
-        (key, value) ->
-            translations.put(
-                key,
-                StringUtils.isBlank(value)
-                    ? StringUtils.EMPTY
-                    : translate(
-                        value,
-                        inputLanguageAndCountry,
-                        outputLanguageAndCountry,
-                        translationEngineParams)));
-    return translations;
-  }
-
-  private String translate(
-      final String entry,
-      final LanguageAndCountry inputLanguageAndCountry,
-      final LanguageAndCountry outputLanguageAndCountry,
-      final TranslationEngineParams translationEngineParams) {
-    String inputLanguage = inputLanguageAndCountry.getLanguage();
-    String outputLanguage = outputLanguageAndCountry.getLanguage();
-    if (Stream.of(inputLanguage, outputLanguage).allMatch(StringUtils::isNotBlank)) {
-      return translate(entry, inputLanguage, outputLanguage, translationEngineParams);
-    }
-    System.out.printf(
-        "Both input and output languages should not be blank; inputLanguage: %s, outputLanguage: %s%n",
-        inputLanguage, outputLanguage);
-    return StringUtils.EMPTY;
-  }
-
-  private String translate(
+  protected String translate(
       final String entry,
       final String inputLanguage,
       final String outputLanguage,
