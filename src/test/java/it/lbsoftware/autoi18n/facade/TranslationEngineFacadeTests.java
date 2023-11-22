e275a3a76e82493463a79544c78d737e8ba280ba
Luca <lb.luca.bonetti@gmail.com>
2023-11-22 22:42:06 +0100

    fix(null-entries): avoid npe when no entries are provided

--- src/test/java/it/lbsoftware/autoi18n/facade/TranslationEngineFacadeTests.java
+++ src/test/java/it/lbsoftware/autoi18n/facade/TranslationEngineFacadeTests.java
@@ -41,4 +41,29 @@ class TranslationEngineFacadeTests {
     assertNotNull(res);
     assertEquals(ExitCode.USAGE, res);
   }
+
+  @Test
+  @DisplayName("Should return usage exit code when there are no entries to translate")
+  void test2() {
+    // Given
+    var translationEngine = TranslationEngine.GOOGLE_CLOUD_TRANSLATION_V3;
+    Map<String, String> params = Collections.emptyMap();
+    Map<String, String> entries = null;
+    var inputLanguageAndCountry = new LanguageAndCountry("it", null);
+    var outputLanguageAndCountries = List.of(new LanguageAndCountry("en", null));
+    var translationEngineFacade =
+        new TranslationEngineFacade(
+            translationEngine,
+            params,
+            entries,
+            inputLanguageAndCountry,
+            outputLanguageAndCountries);
+
+    // When
+    var res = translationEngineFacade.performTranslation();
+
+    // Then
+    assertNotNull(res);
+    assertEquals(ExitCode.USAGE, res);
+  }
 }
